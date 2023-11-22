ab277e744c8323da9c3b2db4efdef3016591cc3d
ASdOliveira <arysson.silva@hotmail.com>
2023-11-22 17:53:06 -0300

    Adding app title in splash screen

--- App/slide_viewer/lib/Components/SplashScreenWidget.dart
+++ App/slide_viewer/lib/Components/SplashScreenWidget.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:animated_splash_screen/animated_splash_screen.dart';
-
 import '../injuriesGroup.dart';
 
 class SplashScreenWidget extends StatelessWidget {
@@ -13,14 +12,30 @@ class SplashScreenWidget extends StatelessWidget {
         home: AnimatedSplashScreen(
             duration: 2500,
             splashIconSize: 500,
-            splash: Image.asset(
-              'assets/images/logo.png',
-              height: 200,
-              width: 205,
+            splash: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Image.asset(
+                  'assets/images/logo.png',
+                  height: 200,
+                  width: 205,
+                ),
+                SizedBox(height: 40),
+                Text(
+                  "Guia de Patologia Oral",
+                  style: TextStyle(
+                      fontSize: 24,
+                      fontFamily: 'Poppins',
+                      fontWeight: FontWeight.w500,
+                      color: Color(0xFFFFFFFF)),
+                  textAlign: TextAlign.center,
+                )
+              ],
             ),
             nextScreen: InjuriesGroup(),
-            splashTransition: SplashTransition.fadeTransition,
-            animationDuration: const Duration(seconds: 2),
+            splashTransition: SplashTransition.scaleTransition,
+            animationDuration: const Duration(seconds: 1),
             backgroundColor: const Color(0xFF672855)));
   }
 }
