0a04d81435b6d7d4fae33977c41b65664bb71405
ASdOliveira <arysson.silva@hotmail.com>
2023-12-05 21:30:24 -0300

    Changing strings and configuring to publish

--- App/slide_viewer/lib/injuriesGroup.dart
+++ App/slide_viewer/lib/injuriesGroup.dart
@@ -26,60 +26,58 @@ class InjuriesGroupState extends State<InjuriesGroup> {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-            backgroundColor: const Color(0xFF672855),
-            title: const SearchWidget()),
-        backgroundColor: const Color(0xFFEAEFF3),
-        drawer: DrawerWidget(),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const Padding(
-              padding: EdgeInsets.all(35.0),
-              child: H1TextWidget(
-                text: "Grupo de Lesões",
-              ),
+    return Scaffold(
+      appBar: AppBar(
+          backgroundColor: const Color(0xFF672855),
+          title: const SearchWidget()),
+      backgroundColor: const Color(0xFFEAEFF3),
+      drawer: DrawerWidget(),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const Padding(
+            padding: EdgeInsets.all(35.0),
+            child: H1TextWidget(
+              text: "Grupo de Lesões",
             ),
-            Expanded(
-              child: GridView.builder(
-                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                      crossAxisCount: 2,
-                      mainAxisSpacing: 16,
-                      crossAxisSpacing: 16,
-                      childAspectRatio: 1.3),
-                  physics: const BouncingScrollPhysics(),
-                  shrinkWrap: true,
-                  padding: const EdgeInsets.fromLTRB(16, 0, 16, 0),
-                  scrollDirection: Axis.vertical,
-                  itemCount: buttons.length,
-                  itemBuilder: (context, index) {
-                    return ElevatedButton(
-                      onPressed: () {
-                        Navigator.push(
-                          context,
-                          PageRouteBuilder(
-                              pageBuilder: (_, __, ___) => InjuriesSubGroup(
-                                  parentId: buttons[index].id,
-                                  parentName: buttons[index].label),
-                              transitionDuration:
-                                  const Duration(milliseconds: 200),
-                              transitionsBuilder: (_, a, __, c) =>
-                                  FadeTransition(opacity: a, child: c)),
-                        );
-                      },
-                      style: customButtonStyle(),
-                      child: H2TextWidget(
-                        text: buttons[index].label,
-                        fontSize: 15,
-                      ),
-                    );
-                  }),
-            ),
-          ],
-        ),
+          ),
+          Expanded(
+            child: GridView.builder(
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    mainAxisSpacing: 16,
+                    crossAxisSpacing: 16,
+                    childAspectRatio: 1.3),
+                physics: const BouncingScrollPhysics(),
+                shrinkWrap: true,
+                padding: const EdgeInsets.fromLTRB(16, 0, 16, 0),
+                scrollDirection: Axis.vertical,
+                itemCount: buttons.length,
+                itemBuilder: (context, index) {
+                  return ElevatedButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        PageRouteBuilder(
+                            pageBuilder: (_, __, ___) => InjuriesSubGroup(
+                                parentId: buttons[index].id,
+                                parentName: buttons[index].label),
+                            transitionDuration:
+                                const Duration(milliseconds: 200),
+                            transitionsBuilder: (_, a, __, c) =>
+                                FadeTransition(opacity: a, child: c)),
+                      );
+                    },
+                    style: customButtonStyle(),
+                    child: H2TextWidget(
+                      text: buttons[index].label,
+                      fontSize: 15,
+                    ),
+                  );
+                }),
+          ),
+        ],
       ),
     );
   }
